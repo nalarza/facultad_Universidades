1ba2a2284f2bf71d4791ecfef641864081d30d7b
Nelson Andres Alarza Andrade <nalarzaandrade@gmail.com>
2023-02-22 06:52:48 -0500

    3er

---
+++ src/main/java/universidades/Nelson/NelsonApplication.java
@@ -0,0 +1,13 @@
+package universidades.Nelson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NelsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NelsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/universidades/Nelson/NelsonApplicationTests.java
@@ -0,0 +1,13 @@
+package universidades.Nelson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NelsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
