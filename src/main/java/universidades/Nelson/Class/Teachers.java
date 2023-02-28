cd8b5a0055e53c6782dbcd5ee6fe6c34995107bd
Nelson Andres Alarza Andrade <nalarzaandrade@gmail.com>
2023-02-28 11:16:47 -0500

    4to commit
    se hacen las tablas en base de datos "MySQL workbench" desde el lenguaje de programación java 11

--- src/main/java/universidades/Nelson/Class/Teachers.java
+++ src/main/java/universidades/Nelson/Class/Teachers.java
@@ -1,11 +1,68 @@
 package universidades.Nelson.Class;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "teacher")
 public class Teachers {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
     private long id_teacher;
     private String names_teacher;
     private String sur_name_teacher;
     private String phone_number_teacher;
     private String email_teacher;
 
+    public Teachers() {
+    }
+
+    public Teachers(long id_teacher, String names_teacher, String sur_name_teacher, String phone_number_teacher, String email_teacher) {
+        this.id_teacher = id_teacher;
+        this.names_teacher = names_teacher;
+        this.sur_name_teacher = sur_name_teacher;
+        this.phone_number_teacher = phone_number_teacher;
+        this.email_teacher = email_teacher;
+    }
+
+    public long getId_teacher() {
+        return id_teacher;
+    }
+
+    public void setId_teacher(long id_teacher) {
+        this.id_teacher = id_teacher;
+    }
+
+    public String getNames_teacher() {
+        return names_teacher;
+    }
+
+    public void setNames_teacher(String names_teacher) {
+        this.names_teacher = names_teacher;
+    }
+
+    public String getSur_name_teacher() {
+        return sur_name_teacher;
+    }
+
+    public void setSur_name_teacher(String sur_name_teacher) {
+        this.sur_name_teacher = sur_name_teacher;
+    }
+
+    public String getPhone_number_teacher() {
+        return phone_number_teacher;
+    }
+
+    public void setPhone_number_teacher(String phone_number_teacher) {
+        this.phone_number_teacher = phone_number_teacher;
+    }
+
+    public String getEmail_teacher() {
+        return email_teacher;
+    }
+
+    public void setEmail_teacher(String email_teacher) {
+        this.email_teacher = email_teacher;
+    }
 }
--- src/main/java/universidades/Nelson/Class/Universities.java
+++ src/main/java/universidades/Nelson/Class/Universities.java
@@ -1,7 +1,13 @@
 package universidades.Nelson.Class;
 
-public class Universities {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "university")
+public class Universities {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
     private long id_university;
     private String name_university;
     private String email_university;
@@ -9,4 +15,73 @@ public class Universities {
     private String phone_number_university;
     private String city_university;
     private String country_university;
+
+    public Universities() {
+    }
+
+    public Universities(long id_university, String name_university, String email_university, String address_university, String phone_number_university, String city_university, String country_university) {
+        this.id_university = id_university;
+        this.name_university = name_university;
+        this.email_university = email_university;
+        this.address_university = address_university;
+        this.phone_number_university = phone_number_university;
+        this.city_university = city_university;
+        this.country_university = country_university;
+    }
+
+    public long getId_university() {
+        return id_university;
+    }
+
+    public void setId_university(long id_university) {
+        this.id_university = id_university;
+    }
+
+    public String getName_university() {
+        return name_university;
+    }
+
+    public void setName_university(String name_university) {
+        this.name_university = name_university;
+    }
+
+    public String getEmail_university() {
+        return email_university;
+    }
+
+    public void setEmail_university(String email_university) {
+        this.email_university = email_university;
+    }
+
+    public String getAddress_university() {
+        return address_university;
+    }
+
+    public void setAddress_university(String address_university) {
+        this.address_university = address_university;
+    }
+
+    public String getPhone_number_university() {
+        return phone_number_university;
+    }
+
+    public void setPhone_number_university(String phone_number_university) {
+        this.phone_number_university = phone_number_university;
+    }
+
+    public String getCity_university() {
+        return city_university;
+    }
+
+    public void setCity_university(String city_university) {
+        this.city_university = city_university;
+    }
+
+    public String getCountry_university() {
+        return country_university;
+    }
+
+    public void setCountry_university(String country_university) {
+        this.country_university = country_university;
+    }
 }
