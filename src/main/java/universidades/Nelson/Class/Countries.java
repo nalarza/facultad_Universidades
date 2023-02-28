1bbaeac57a311c5f660d7344f7d30b9e4ae4d071
Nelson Andres Alarza Andrade <nalarzaandrade@gmail.com>
2023-02-28 11:48:49 -0500

    relaciono las tablas #1
    se hace la relacion de tablas entre la tabal de pais y universidades

--- src/main/java/universidades/Nelson/Class/Countries.java
+++ src/main/java/universidades/Nelson/Class/Countries.java
@@ -2,13 +2,55 @@ package universidades.Nelson.Class;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "country")
 public class Countries {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(unique = true, nullable = false)
-    private long id_country;
+    @Column(name = "id_country",unique = true, nullable = false)
+    private long id;
     private String name_country;
+
+    @OneToMany(mappedBy = "country",cascade = CascadeType.ALL)
+    private Set<Universities> universities = new HashSet<>();
+
+    public Countries() {
+    }
+
+    public Countries(long id, String name_country, Set<Universities> universities) {
+        this.id = id;
+        this.name_country = name_country;
+        this.universities = universities;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName_country() {
+        return name_country;
+    }
+
+    public void setName_country(String name_country) {
+        this.name_country = name_country;
+    }
+
+    public Set<Universities> getUniversities() {
+        return universities;
+    }
+
+    public void setUniversities(Set<Universities> universities) {
+        this.universities = universities;
+        for (Universities university: universities){
+            university.setCountry(this);
+        }
+    }
 }
